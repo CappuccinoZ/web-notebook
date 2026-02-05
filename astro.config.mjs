4ba1aaef3d17c7e864dbd9eaa9cf480ab7544228
CappuccinoZ <zq0410@outlook.com>
2026-02-05 17:56:03 +0800

    创建项目

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkBreaks from 'remark-breaks'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Web笔记',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'HTML5 笔记',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'HTML5 语义标签', slug: 'html5' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'CSS3 笔记',
+					items: [
+						{ label: 'CSS3 核心技术', slug: 'css3/1' },
+						{ label: '现代网页布局', slug: 'css3/2' },
+						{ label: '交互动效设计', slug: 'css3/3' },
+						{ label: '前沿技术拓展', slug: 'css3/4' },
+						{ label: '移动网页与响应网页开发', slug: 'css3/5' },
+					],
+				},
+				{
+					label: 'Javascript 笔记',
+					items: [
+						{ label: 'JS 基础', slug: 'javascript/1' },
+						{ label: 'WEB APIs', slug: 'javascript/2' },
+						{ label: 'jQuery 笔记', slug: 'javascript/jquery' },
+					],
 				},
 			],
 		}),
 	],
+	markdown: {
+		remarkPlugins: [remarkBreaks]
+	}
 });
